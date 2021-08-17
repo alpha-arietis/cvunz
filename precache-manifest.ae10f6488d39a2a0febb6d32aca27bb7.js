c489eca4eac2111a546b7b26f161a42c85cc787f
Ivan Aleksandrov <58612800+alpha-arietis@users.noreply.github.com>
2021-08-17 18:18:44 +1200

    Fixed country selector

---
+++ precache-manifest.ae10f6488d39a2a0febb6d32aca27bb7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f902d5fb98db31cc787b27e60bb181a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbfadaf7f299352bcc79",
+    "url": "/static/css/main.96306ba5.chunk.css"
+  },
+  {
+    "revision": "0bafad0c8722cf20bfd6",
+    "url": "/static/js/2.33e2837d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.33e2837d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbfadaf7f299352bcc79",
+    "url": "/static/js/main.23f57c5e.chunk.js"
+  },
+  {
+    "revision": "0409c83a40f4ef27031a",
+    "url": "/static/js/runtime-main.ede661df.js"
+  },
+  {
+    "revision": "2e5e7e90bcbc339d0921817a2a189d3b",
+    "url": "/static/media/2958.2e5e7e90.jpg"
+  },
+  {
+    "revision": "feaad23513636b91f2a46ec8665ea427",
+    "url": "/static/media/3173.feaad235.jpg"
+  },
+  {
+    "revision": "c86f306ee509c88e4c8de5f0619a2ffe",
+    "url": "/static/media/alphaarietis.c86f306e.png"
+  },
+  {
+    "revision": "a2f2b96684d0a35cab4f0122a3617cef",
+    "url": "/static/media/information.a2f2b966.png"
+  },
+  {
+    "revision": "cb0b2bd2282ecf5d57938ce7aaf22267",
+    "url": "/static/media/radar-512.cb0b2bd2.png"
+  }
+]);
